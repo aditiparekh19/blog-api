a5c2006243f450492675b01fd7ec1d9e19ab6d1a
aditi-parekh <aditi.parekh.196@gmail.com>
2025-07-15 23:27:44 +0530

    Login functionality

---
+++ src/controllers/v1/auth/login.ts
@@ -0,0 +1,70 @@
+import { generateAccessToken, generateRefreshToken } from '@/lib/jwt';
+import { logger } from '@/lib/winston';
+import config from '@/config';
+
+import User from '@/models/user';
+import Token from '@/models/token';
+
+import type { Request, Response } from 'express';
+import type { IUser } from '@/models/user';
+
+type UserData = Pick<IUser, 'email' | 'password'>;
+
+const login = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email } = req.body as UserData;
+    const user = await User.findOne({ email })
+      .select('username email password role')
+      .lean()
+      .exec();
+
+    if (!user) {
+      res.status(404).json({
+        code: 'NotFound',
+        message: 'Usernot found',
+      });
+      return;
+    }
+
+    // Generate access token and refresh token for new user
+    const accessToken = generateAccessToken(user._id);
+    const refreshToken = generateRefreshToken(user._id);
+
+    // Store refresh token in DB
+    await Token.create({
+      token: refreshToken,
+      userId: user._id,
+    });
+    logger.info('Refresh token created for user', {
+      userId: user._id,
+      token: refreshToken,
+    });
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: config.NODE_ENV === 'production',
+      sameSite: 'strict',
+    });
+
+    res.status(201).json({
+      user: {
+        username: user.username,
+        email: user.email,
+        role: user.role,
+      },
+      accessToken,
+    });
+
+    logger.info('User logged in successfully', user);
+  } catch (err) {
+    res.status(500).json({
+      code: 'SeverError',
+      message: 'Internal server error',
+      error: err,
+    });
+
+    logger.error('Error during user login', err);
+  }
+};
+
+export default login;
